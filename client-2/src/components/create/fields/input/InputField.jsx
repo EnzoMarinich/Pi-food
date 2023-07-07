9c0744260244646dd302bd8e41821da3e1a00eaa
EnzoMarinich <marinichenzo@gmail.com>
2023-07-07 17:46:14 -0300

    form

--- client-2/src/components/create/fields/input/InputField.jsx
+++ client-2/src/components/create/fields/input/InputField.jsx
@@ -6,7 +6,7 @@ export const InputField = ({label, type, name, handleInput, formData, error}) =>
   return (
     <div className={style[name]}>
         <span>{label} {name == "healthScore"? formData.healthScore : null}</span>
-        {name== "steps"? <textarea name={name} onChange={handleInput}></textarea> : <input autocomplete="off"  value={formData[name]} name={name} onChange={handleInput} type={type} /> }
+        {name== "steps"? <textarea name={name} onChange={handleInput}></textarea> : <input  autocomplete="off"  value={formData[name]} name={name} onChange={handleInput} type={type} /> }
         {error?  <span className={style.error}>{error}</span> : null}
     </div>
   )
--- client-2/src/utils/validateForm.js
+++ client-2/src/utils/validateForm.js
@@ -1,3 +1,4 @@
+import { formToJSON } from "axios"
 
 
 export  const validateForm = (formData, recipes)=>{
@@ -8,6 +9,8 @@ export  const validateForm = (formData, recipes)=>{
     if(recipes.some((recipe) => recipe.name == formData.name)) errors.name = "name already exists"
     if(formData.summary == "")errors.summary = "summary is required"
     if(formData.healthScore == "") errors.healthScore = "healthScore is required"
+    if(!(Number(formData.healthScore)))errors.healthScore = "healthScore requires a number"
+    if(Number(formData.healthScore) > 100 || Number(formData.healthScore) < 0 ) errors.healthScore = "healthScore is < 100 y > 0 " 
     if(formData.diets.length == 0) errors.diets = "min. one diet is required"
     if(formData.steps == "") errors.steps = "steps is required"
 
