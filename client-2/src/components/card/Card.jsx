a09c824a65e5ad0f40ec58e51e51b838ac078de3
EnzoMarinich <marinichenzo@gmail.com>
2023-07-11 04:40:31 -0300

    edit css

--- client-2/src/components/card/Card.jsx
+++ client-2/src/components/card/Card.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import style from "./Card.module.css"
-import { Link } from 'react-router-dom'
-import {FiChevronsUp} from "react-icons/fi"
+import React from "react";
+import style from "./Card.module.css";
+import { Link } from "react-router-dom";
+import { FiChevronsUp } from "react-icons/fi";
 
-export const Card = ({recipe}) => {
+export const Card = ({ recipe }) => {
+  let { diets, image } = recipe;
 
-  let {diets, image} = recipe
-
-  const img = image? image : "https://img.freepik.com/vector-gratis/coleccion-platos-comida-dibujados-mano_23-2148062105.jpg?w=740&t=st=1688498596~exp=1688499196~hmac=47f15d7a99dedbc278ca624c168a3ca966332249771636c991ac5f6fa1456f2a"
+  const img = image
+    ? image
+    : "https://img.freepik.com/vector-gratis/coleccion-platos-comida-dibujados-mano_23-2148062105.jpg?w=740&t=st=1688498596~exp=1688499196~hmac=47f15d7a99dedbc278ca624c168a3ca966332249771636c991ac5f6fa1456f2a";
 
   return (
     <div key={recipe.id} className={style.card}>
-      <div className={style.img} style={{backgroundImage: `url(${img})`}}></div>
+      <div
+        className={style.img}
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
       <div className={style.detail}>
         <p className={style.detailName}>{recipe.name}</p>
-      <div className={style.decoration}>
-        <FiChevronsUp className={style.icon}/>
-        <div className={style.detailDiets}>
-        <p>Diets:</p> 
-        <strong>{diets}</strong>
+        <div className={style.detailInfo}>
+          <FiChevronsUp className={style.icon} />
+          <div className={style.detailDiets}>
+            {diets.map(diet=>{
+              let newDiet = diet.charAt(0).toUpperCase() + diet.slice(1)
+              return <strong>{newDiet}; </strong>
+            } )}
+          </div>
+          <Link className={style.link} to={`detail/${recipe.id}`}>
+            <button className={style.button}>more info</button>
+          </Link>
         </div>
-      <Link to={`detail/${recipe.id}`}><button className={style.button}>more info</button></Link>
       </div>
-      </div>
-    </div>
-  )
-}
+    </div> 
+  );
+};
