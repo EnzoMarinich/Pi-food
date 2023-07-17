fa084081ba28618a0a13d7d916910c15313fb008
EnzoMarinich <marinichenzo@gmail.com>
2023-07-16 22:23:56 -0300

    create post

--- client-2/src/utils/createRecipe.js
+++ client-2/src/utils/createRecipe.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const createRecipe = async(data) => {
     try {
-        const rsta = await axios.post("http://localhost:3001/recipes", data)
+        const rsta = await axios.post("/recipes", data)
         console.log(rsta.data)
     } catch (error) {
         return alert(error)
