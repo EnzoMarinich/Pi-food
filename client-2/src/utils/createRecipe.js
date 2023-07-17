f36e4d3043ecf0b01837b9cee2e8f9c0aa7d5199
EnzoMarinich <marinichenzo@gmail.com>
2023-07-16 23:00:18 -0300

    prueba img

--- client-2/src/utils/createRecipe.js
+++ client-2/src/utils/createRecipe.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 
 export const createRecipe = async(data) => {
+    if(!data.image){
+        data.image = "https://media.istockphoto.com/id/696864806/es/foto/bistec-a-la-plancha-con-verduras-a-la-parrilla-cerveza-y-vino-en-una-mesa-de-madera-oscura.jpg?s=612x612&w=0&k=20&c=UyeqcoHn8kBX9fYGOSBRbJFSnGsV8kNEVFOSuZ1yYdU="
+    }
     try {
         const rsta = await axios.post("/recipes", data)
-        console.log(rsta.data)
+        console.log(rsta)
     } catch (error) {
         return alert(error)
     }
